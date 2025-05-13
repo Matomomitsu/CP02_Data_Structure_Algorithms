567fed1f15d5917367099068c69190e77fdac60b
Mateus Tomomitsu <matomomitsu@gmail.com>
2025-05-12 23:21:06 -0300

    add Verificar Substring

---
+++ main.c
@@ -0,0 +1,102 @@
+// Mateus Scandiuzzi Valente Tomomitsu RM 561565
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <locale.h>
+#include <string.h>
+
+char    get_number(void)
+{
+    char input[3];
+
+    printf("Digite a sua escolha: \t");
+    fgets(input, 3, stdin);
+
+    if (input[1] != '\n' && input[1] != '\0')
+    {
+        printf("Entrada inválida\n");
+        while(fgetc(stdin) != '\n');
+        return('\0');
+    }
+    return (input[0]);
+}
+
+void    check_substring(void)
+{
+    char str1[101];
+    char str2[101];
+    int i = 0;
+    int j;
+
+    printf("Digite a primeira string:\t");
+    fgets(str1, sizeof(str1), stdin);
+    str1[strcspn(str1, "\n")] = 0;
+    if (strcspn(str1, "\0") == 100)
+    {
+        while(fgetc(stdin) != '\n');
+    }
+    printf("Digite a segunda string:\t");
+    fgets(str2, sizeof(str2), stdin);
+    str2[strcspn(str2, "\n")] = 0;
+    if (strcspn(str2, "\0") == 100)
+    {
+        while(fgetc(stdin) != '\n');
+    }
+
+    while(str1[i] != '\0')
+    {
+        j = 0;
+        while (str2[j] != '\0')
+        {
+            if (str1[i + j] != str2[j])
+            {
+                break;
+            }
+            j++;
+        }
+        if (str2[j] == '\0')
+        {
+            printf("A segunda string está contida na primeira.\n");
+            return;
+        }
+        i++;
+    }
+
+    printf("A segunda string NÃO está contida na primeira.\n");
+}
+
+int main()
+{
+    bool continue_loop = true;
+    char number;
+
+    setlocale(LC_ALL, "Portuguese");
+    while(continue_loop)
+    {
+        printf("===== MENU DE EXERCÍCIOS =====\n1 - Sequência de Fibonacci;\n2 - Fatoriais;\n3 - Verificar Palíndromo;\n4 - Verificar Substring.\n5 - Sair.\n");
+        number = get_number();
+        if (number == '\0')
+            continue;
+        switch(number)
+        {
+            case '1':
+                break;
+            case '2':
+                break;
+            case '3':
+                break;
+            case '4':
+                check_substring();
+                break;
+            case '5':
+                continue_loop = false;
+                break;
+            default:
+                printf("Entrada inválida\n");
+                break;
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
